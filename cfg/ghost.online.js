ac42e1ab2c1951df8afb8a6618954f3b43305f76
fwh <ad_fwh@83d509f7-df6f-4f98-bcfb-715ae52eecfb>
2016-11-04 11:22:16 +0000

    修改线上配置
    git-svn-id: svn://192.168.18.251/front-project/webwater@23024 83d509f7-df6f-4f98-bcfb-715ae52eecfb

--- cfg/ghost.online.js
+++ cfg/ghost.online.js
@@ -1,16 +1,15 @@
 var log4js = require('log4js');
-
 log4js.loadAppender("dateFile");
 log4js.addAppender(log4js.appenderMakers['dateFile']({
-    filename: "/home/u1/logs/webwater/webwater.log",
+    filename: "/home/u1/logs/webwater/webwater",
     pattern: '.yyyy-MM-dd.log', alwaysIncludePattern: true,
     layout: {
         type: 'pattern',
         pattern: '[%d %p %c] %m%n'
     }
-}), 'water');
+}), 'webwater');
 
-const logger = log4js.getLogger('water');
+const logger = log4js.getLogger('webwater');
 
 module.exports = {
     get logger() {
@@ -18,8 +17,8 @@ module.exports = {
     },
     dscm: {
         water: {
-            addr: '192.168.18.86',
-            port: 5521
+            addr: '172.16.0.3',
+            port: 5538
         }
     }
 }
