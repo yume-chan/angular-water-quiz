ab8ab2222d23c13cdc13624d4875da3bc0ebefbf
Simon Chan <cnsimonchan@live.com>
2016-11-28 08:43:30 +0000

    统一 ghost.js
    git-svn-id: svn://192.168.18.251/front-project/webwater@24996 83d509f7-df6f-4f98-bcfb-715ae52eecfb

--- cfg/ghost.online.js
+++ cfg/ghost.online.js
@@ -1,8 +1,10 @@
 var log4js = require('log4js');
+
 log4js.loadAppender("dateFile");
 log4js.addAppender(log4js.appenderMakers['dateFile']({
     filename: "/home/u1/logs/webwater/webwater",
-    pattern: '.yyyy-MM-dd.log', alwaysIncludePattern: true,
+    pattern: '.yyyy-MM-dd.log',
+    alwaysIncludePattern: true,
     layout: {
         type: 'pattern',
         pattern: '[%d %p %c] %m%n'
--- cfg/ghost.prev.js
+++ cfg/ghost.prev.js
@@ -2,15 +2,16 @@ var log4js = require('log4js');
 
 log4js.loadAppender("dateFile");
 log4js.addAppender(log4js.appenderMakers['dateFile']({
-    filename: "/home/u1/logs/webwater/webwater.log",
-    pattern: '.yyyy-MM-dd.log', alwaysIncludePattern: true,
+    filename: "/home/u1/logs/webwater/webwater",
+    pattern: '.yyyy-MM-dd.log',
+    alwaysIncludePattern: true,
     layout: {
         type: 'pattern',
         pattern: '[%d %p %c] %m%n'
     }
-}), 'water');
+}), 'webwater');
 
-const logger = log4js.getLogger('water');
+const logger = log4js.getLogger('webwater');
 
 module.exports = {
     get logger() {
